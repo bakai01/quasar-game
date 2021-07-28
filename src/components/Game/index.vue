286d2c83fb0667fa3c8f6753d70a5e6d3882601d
bakai <thedesport20@gmail.com>
2021-07-28 17:03:43 +0600

    moved game block to component

---
+++ src/components/Game/index.vue
@@ -0,0 +1,78 @@
+<template>
+  <div class="game">
+    <div v-for="category in getQuestions" :key="category.id" class="game-row">
+      <span>{{ category.title }}</span>
+
+      <div class="game-row__right">
+        <button
+          v-for="question in category.clues"
+          :key="question.id"
+          class="game-row__btn"
+          @click="emitQuestion(category.id, question.id)"
+          :disabled="!question.value"
+        >
+          {{ question.value }}
+        </button>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import { mapGetters } from "vuex"
+
+export default {
+  name: "Game",
+  computed: {
+    ...mapGetters('storeGame', ['getQuestions'])
+  },
+  methods: {
+    emitQuestion(categoryId, questionId) {
+      this.$emit('choiceQuestion', { categoryId, questionId })
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.game {
+  padding: 150px;
+
+  &-row {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    color: $blue-grey-10;
+
+    span {
+      font-size: 1.5rem;
+      font-weight: 500;
+      text-transform: uppercase;
+    }
+
+    &__right {
+      margin-left: 25px;
+      display: flex;
+    }
+
+    &__btn {
+      border: 1px solid rgb(128, 128, 128);
+      font-size: 1rem;
+      cursor: pointer;
+      margin: 1px;
+      width: 70px;
+      height: 70px;
+      background-color: $green-13;
+      transition: all .2s linear;
+
+      &:hover {
+        background-color: darken($green-13, 3);
+      }
+
+      &:active {
+        background-color: darken($green-13, 6);
+      }
+    }
+  }
+}
+</style>
--- src/pages/Game.vue
+++ src/pages/Game.vue
@@ -1,23 +1,7 @@
 <template>
   <q-page class="flex flex-center column">
 
-    <div class="game">
-      <div v-for="category in getQuestions" :key="category.id" class="game-row">
-        <span>{{ category.title }}</span>
-
-        <div class="game-row__right">
-          <button
-            v-for="question in category.clues"
-            :key="question.id"
-            class="game-row__btn"
-            @click="pickQuestion(category.id, question.id)"
-            :disabled="!question.value"
-          >
-            {{ question.value }}
-          </button>
-        </div>
-      </div>
-    </div>
+    <Game @choiceQuestion="pickQuestion" />
 
     <q-dialog v-model="popupQuestion" persistent>
       <q-card style="min-width: 350px">
@@ -38,6 +22,22 @@
       </q-card>
     </q-dialog>
 
+     <q-dialog v-model="alert">
+      <q-card>
+        <q-card-section>
+          <div class="text-h6">Alert</div>
+        </q-card-section>
+
+        <q-card-section class="q-pt-none">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Rerum repellendus sit voluptate voluptas eveniet porro. Rerum blanditiis perferendis totam, ea at omnis vel numquam exercitationem aut, natus minima, porro labore.
+        </q-card-section>
+
+        <q-card-actions align="right">
+          <q-btn flat label="OK" color="primary" @click="closeAlert" />
+        </q-card-actions>
+      </q-card>
+    </q-dialog>
+
     <q-btn
       color="black"
       label="Начать игру"
@@ -51,10 +51,14 @@
 <script>
 import { mapActions, mapGetters, mapMutations } from "vuex"
 
+import Game from "components/Game"
+
 export default {
-  name: "PageIndex",
+  name: "Main",
+  components: { Game }, 
   data: () => ({
     popupQuestion: false,
+    alert: false,
     address: "",
     countTimer: 60,
     timer: null
@@ -74,9 +78,7 @@ export default {
         this.fetchQuestions()
       }
     },
-    pickQuestion(categoryId, questionId) {
-      const args = { categoryId, questionId }
-
+    pickQuestion(args) {
       this.setCurrentQuestion(args)
       this.openPopupQuestion()
       this.setTimer()
@@ -90,13 +92,18 @@ export default {
     },
     openPopupQuestion() {
       this.popupQuestion = true
+    },
+    closeAlert() {
+      this.alert = false
+    },
+    openAlert() {
+      this.alert = true
     }
   },
   computed: {
     ...mapGetters("storeGame", [
       "getToggleDisableBtn",
       "getCategories",
-      "getQuestions",
       "getCurrentQuestion"
     ])
   }
@@ -104,47 +111,6 @@ export default {
 </script>
 
 <style lang="scss" scoped>
-.game {
-  padding: 150px;
-
-  &-row {
-    display: flex;
-    justify-content: space-between;
-    align-items: center;
-    color: $blue-grey-10;
-
-    span {
-      font-size: 1.5rem;
-      font-weight: 500;
-      text-transform: uppercase;
-    }
-
-    &__right {
-      margin-left: 25px;
-      display: flex;
-    }
-
-    &__btn {
-      border: 1px solid rgb(128, 128, 128);
-      font-size: 1rem;
-      cursor: pointer;
-      margin: 1px;
-      width: 70px;
-      height: 70px;
-      background-color: $green-13;
-      transition: all .2s linear;
-
-      &:hover {
-        background-color: darken($green-13, 3);
-      }
-
-      &:active {
-        background-color: darken($green-13, 6);
-      }
-    }
-  }
-}
-
 .text-h4 {
   padding-bottom: 35px;
   font-weight: 600;
