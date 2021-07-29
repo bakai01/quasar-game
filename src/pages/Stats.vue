d0527bc39e583768a0227d39a22e2ea604599ac0
bakai <thedesport20@gmail.com>
2021-07-29 14:54:28 +0600

    stats component is done

--- src/pages/Stats.vue
+++ src/pages/Stats.vue
@@ -1,15 +1,36 @@
 <template>
-  <h1>
-      Stats
-  </h1>
+  <div class="stats">
+    <p><strong>Your name:</strong> {{ getPlayerName }}</p>
+    <p><strong>Total points:</strong> {{ getPoints }}</p>
+    <p><strong>Total answered questions:</strong> {{ getTotalAnswers }}</p>
+    <p><strong>Right answers:</strong> {{ getRightAnswers }}</p>
+    <p><strong>Wrong answers:</strong> {{ getWrongAnswers }}</p>
+  </div>
 </template>
 
 <script>
-export default {
+import { mapGetters } from "vuex"
 
+export default {
+  name: 'Stats',
+  computed: {
+    ...mapGetters('storeGame', [
+      'getPoints',
+      'getPlayerName',
+      'getTotalAnswers',
+      'getRightAnswers',
+      'getWrongAnswers'
+    ])
+  }
 }
 </script>
 
 <style lang="scss" scoped>
-
+.stats {
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  font-size: 1.6rem;
+}
 </style>
