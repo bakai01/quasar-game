aa1ba59a537cbafe8e01dad579fb725f6509dd96
bakai <thedesport20@gmail.com>
2021-08-02 17:59:42 +0600

    w/e

--- src/store/modules/users.js
+++ src/store/modules/users.js
@@ -2,12 +2,40 @@
 const state = () => ({
   users: [
     {
-      playerName: "",
-      totalAnswers: 0,
-      rightAnswers: 0,
+      playerName: "ZXC Dead inside 1000 - 7, Как же ты слаб))",
+      points: 5000,
+      rightAnswers: 10,
+      totalAnswers: 10,
+      wrongAnswers: 0
+    },
+    {
+      playerName: "Alan",
+      totalAnswers: 1,
+      rightAnswers: 1,
       wrongAnswers: 0,
-      points: 0
-    }
+      points: 400
+    },
+    {
+      playerName: "Petya",
+      totalAnswers: 3,
+      rightAnswers: 2,
+      wrongAnswers: 1,
+      points: 600
+    },
+    {
+      playerName: "Bart",
+      totalAnswers: 7,
+      rightAnswers: 5,
+      wrongAnswers: 2,
+      points: 2500
+    },
+    {
+      playerName: "Alesia",
+      totalAnswers: 10,
+      rightAnswers: 2,
+      wrongAnswers: 8,
+      points: 50000
+    },
   ]
 })
 
